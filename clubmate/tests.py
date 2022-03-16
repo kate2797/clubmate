737e3b318b333d5c886d1e519fce4391d9573d3c
kate2797 <katka.zaprazna@icloud.com>
2022-03-16 19:56:47 +0000

    Unit tests and random fixes

--- clubmate/tests.py
+++ clubmate/tests.py
@@ -4,18 +4,17 @@ from django.urls import reverse
 from clubmate.models import Club
 
 
-def create_dummy_club(name):
-    club = Club.objects.get_or_create(name=name, club_description='test', city='test', website_url='https://swg3.tv/',
-                                      genre='test', location_coordinates='test', opening_hours_week='test',
-                                      opening_hours_weekend='test', picture='event_pictures/default_event.png')[0]
-    return club
-
-
 class ClubModelTests(TestCase):
-    """ Ensures that the default value for entry fee is zero. """
+    def setUp(self):
+        club = \
+            Club.objects.get_or_create(name='Test', club_description='Test', city='Test',
+                                       website_url='https://swg3.tv/',
+                                       genre='Test', location_coordinates='Test', opening_hours_week='Test',
+                                       opening_hours_weekend='Test')[0]
 
     def test_ensure_min_entry_free_is_zero(self):
-        club = create_dummy_club('SWG3')
+        """ Ensures that the default value for entry fee is zero. """
+        club = Club.objects.get_or_create(name='Test')[0]
         self.assertEqual((club.entry_fee == 0.0), True)
 
 
@@ -29,4 +28,28 @@ class ClubDetailViewTests(TestCase):
 
 
 class DiscoverViewTests(TestCase):
-    pass
+    def setUp(self):
+        inn_deep = \
+            Club.objects.get_or_create(name='Inn Deep', club_description='Test', city='Test',
+                                       website_url='https://swg3.tv/',
+                                       genre='Test', location_coordinates='Test', opening_hours_week='Test',
+                                       opening_hours_weekend='Test')[0]
+
+        swg3 = \
+            Club.objects.get_or_create(name='SWG3', club_description='Test', city='Test',
+                                       website_url='https://swg3.tv/',
+                                       genre='Test', location_coordinates='Test', opening_hours_week='Test',
+                                       opening_hours_weekend='Test')[0]
+
+    def test_clubs_display_correctly_on_discover(self):
+        """ Checks if clubs are correctly retrieved from the database on Discover. """
+        count = Club.objects.count()
+        self.assertEqual(count, 2)
+
+
+class IndexViewTests(TestCase):
+    def test_index_subheading(self):
+        """ Checks if the HTML subheading of the homepage is correctly displayed. """
+        response = self.client.get(reverse('clubmate:index'))
+        subheading = '<h4 class="mt-3 mb-3">Student clubbing made fun, safe and easy 🥤</h4>' in response.content.decode()
+        self.assertTrue(subheading)
