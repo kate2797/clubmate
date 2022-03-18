241c37c7bf70448525c73a285fee08f8031dac11
FlameChan-s <2613041s@student.gla.ac.uk>
2022-03-18 14:11:00 +0000

    update filter for rating page

--- static/js/script.js
+++ static/js/script.js
@@ -125,14 +125,22 @@ function appendDiv(array, parent) {
 function rateDisplay(condition) {
     if (condition === "default_display") {
         let displayDiv = document.getElementById("default_display")
-        displayDiv.style.display = "none";
-        let hideDiv = document.getElementById("upvote_display")
-        hideDiv.style.display = "";
-    }
-    if (condition === "upvote_display") {
-        let displayDiv = document.getElementById("default_display")
         displayDiv.style.display = "";
         let hideDiv = document.getElementById("upvote_display")
         hideDiv.style.display = "none";
+        let ele = document.getElementById("dropdownMenuButton1")
+        ele.innerText = "Published Time"
     }
+    if (condition === "upvote_display") {
+        let displayDiv = document.getElementById("default_display")
+        displayDiv.style.display = "none";
+        let hideDiv = document.getElementById("upvote_display")
+        hideDiv.style.display = "";
+        let ele = document.getElementById("dropdownMenuButton1")
+        ele.innerText = "Upvote"
+    }
+}
+
+function rateOrder(condition) {
+
 }
