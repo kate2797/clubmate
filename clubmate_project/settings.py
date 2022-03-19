635d4aa748495fe5b67726453a959c5bdcf94da3
mariosanas <2200644a@student.gla.ac.uk>
2022-03-19 23:37:21 +0000

    added the deployed website as an allowed host

--- clubmate_project/settings.py
+++ clubmate_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','itech33.pythonanywhere.com']
 
 # Application definition
 
